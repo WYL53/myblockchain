09068a87f9b70724b8f89460b4d3025e53aee394
WYL53 <843794313@qq.com>
2018-03-16 11:39:23 +0800

    init

---
+++ block.go
@@ -0,0 +1,16 @@
+package block
+
+import (
+	"strconv"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timeStamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+}
